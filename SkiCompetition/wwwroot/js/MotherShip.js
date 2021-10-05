1c41bef4fa935d56c70a12bcb4386b15a412de8d
Eleonora Doseva <Eleonora.Doseva@docuware.com>
2021-10-05 18:36:59 +0300

    Add project files.

---
+++ SkiCompetition/wwwroot/js/MotherShip.js
@@ -0,0 +1,25 @@
+class ContentVM {
+    constructor(Name) {
+        this.Name = Name;
+        this.templateName = Name + "-template";
+    }
+}
+class Deck {
+    constructor(Name) {
+        this.Name = Name;
+        this.contentVM = new ContentVM(Name);
+    }
+}
+export default class MotherShipVM {
+    constructor() {
+        this.Decks = [
+            new Deck("Competitions"),
+            new Deck("Competitors"),
+            new Deck("Teams"),
+            new Deck("Personal ranks"),
+            new Deck("Team ranks")
+        ];
+        this.ActiveDeck = ko.observable(this.Decks[0]);
+    }
+}
+//# sourceMappingURL=MotherShip.js.map
---
+++ SkiCompetition/wwwroot/scripts/MotherShip.ts
@@ -0,0 +1,30 @@
+﻿
+class ContentVM {
+    public templateName: string;
+    constructor(public Name: string) {
+        this.templateName = Name + "-template";
+    }
+}
+
+class Deck {
+    public contentVM: ContentVM;
+    
+    constructor(public Name: string) {
+        this.contentVM = new ContentVM(Name);
+    }
+}
+
+export default class MotherShipVM {
+    Decks: Array<Deck>;
+    ActiveDeck: KnockoutObservable<Deck>;
+    constructor() {
+        this.Decks = [
+            new Deck("Competitions"),
+            new Deck("Competitors"),
+            new Deck("Teams"),
+            new Deck("Personal ranks"),
+            new Deck("Team ranks")
+        ];
+        this.ActiveDeck = ko.observable(this.Decks[0]);
+    }
+}
